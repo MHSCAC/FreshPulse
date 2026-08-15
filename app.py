2216768f25c6e40dc1c6c515daa226d5775366fb
Sai Belde <saibelde@Mac-2625.lan>
2026-08-14 20:59:59 -0400

    Swtiched to 2.5 pro model

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def aircpt(image):
     #Basically asked AI to give the app the item name, a deisgnated emoji, a lifetime, the carbs, the protein, the fat, and sodium, of each item on the user's receipt
     try:
         response = client.models.generate_content(
-        model='gemini-3.5-flash', #Cant use 2.5 flash, google retired it for new users
+        model='gemini-2.5-pro', #Cant use 2.5 flash, google retired it for new users
         contents=[image,aiprompt],#This makes the code send the user's image and our prompt to Gemini
 config=types.GenerateContentConfig(response_mime_type="application/json")
         )
